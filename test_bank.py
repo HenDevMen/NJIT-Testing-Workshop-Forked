e2d2d2451b84142c1638ab750ccb8d8a6ca2e83e
HenDevMen <41549610+HenDevMen@users.noreply.github.com>
2024-01-30 09:54:04 -0500

    Update test_bank.py

--- test_bank.py
+++ test_bank.py
@@ -7,7 +7,3 @@ def test_view_balance_success():
     actual_balance = bank_account.view_balance()
     assert actual_balance == 100
 
-def test_view_balance_unsuccessful():
-    bank_account = BankAccount(username='name', balance=-100)
-    with pytest.raises(BalanceRetrievalException):
-        bank_account.view_balance()
