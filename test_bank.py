0ffb3489d693399ee0b7f742d705afb512b94daf
Uday Patel <uday3patel@gmail.com>
2024-04-04 12:03:52 -0400

    completed withdraw function and TDD coverage tests

--- test_bank.py
+++ test_bank.py
@@ -18,3 +18,25 @@ def test_deposit_function():
     bank_account.deposit(10)
 
     assert bank_account.view_balance() == 110
+
+def test_withdraw_function_successful():
+    bank_account = BankAccount(username='name', balance=100)
+
+    bank_account.withdraw(75)
+
+    assert bank_account.view_balance() == 25
+
+def test_withdraw_function_overdraft():
+    bank_account = BankAccount(username='name', balance=100)
+
+    bank_account.withdraw(150)
+
+    assert bank_account.view_balance() == WithdrawalException
+
+def test_withdraw_function_unsuccessful():
+
+    bank_account = BankAccount(username='name', balance=100)
+
+    bank_account.withdraw(-50)
+
+    assert bank_account.view_balance() == WithdrawalException
